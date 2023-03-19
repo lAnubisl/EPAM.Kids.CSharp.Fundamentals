f0e6b5f68589d4b4393baaf8d462cfb8c4e8a460
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2023-03-19 10:51:55 +0000

    add Lesson_02

---
+++ Lesson_02/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+AskUser("What is the capital of France?", "Paris", 3);
+
+
+bool AskUser(string question, string correctAnswer, int tries)
+{
+    Console.WriteLine(question);
+    string userAnswer = Console.ReadLine();
+    if (userAnswer == correctAnswer) {
+        Console.WriteLine("Correct");
+        return true;
+    } else {
+        Console.WriteLine("Incorrect");
+        if (tries == 1) {
+            Console.WriteLine("You have run out of tries");
+            return false;
+        }
+
+        return AskUser(question, correctAnswer, tries - 1);
+    }
+}
